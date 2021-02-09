7da16d97e0209eeec6e16fc8810bac138b556ed7
Jack <jack.dillon1@pcc.edu>
2021-02-08 21:01:16 -0800

    prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -4,4 +4,7 @@ package edu.pcc.cis.helloworld;
  * Jack Dillon 02.08.21
  */
 public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
 }
